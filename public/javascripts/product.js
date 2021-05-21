3c59834e96d2be22fd4e91802509a199d2e95d94
许永健 <1942049754@qq.com>
2021-05-21 22:33:51 +0800

    delete

---
+++ public/javascripts/product.js
@@ -0,0 +1,14 @@
+$(function(){
+    Array.from($('.del')).forEach(i =>
+        i.onclick = function(){
+            let id = $(i).parent().parent().attr("datakey");
+            $.ajax({
+                url:"/table1/del/"+id,
+                type:"DELETE",
+                success:function(){
+                        alert("删除成功");
+                        $(i).parent().parent().remove();
+                }
+            });
+        });
+});
--- routes/table1.js
+++ routes/table1.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var connection = require('./conmysql')
 
 
-let show_sql = 'SELECT product_img,product_name,category_name,details,price,sales_count,inventory,product.create_time FROM product INNER JOIN classify INNER JOIN inventory ON product.classify_id = classify.id AND product.id = inventory.product_id';
+let show_sql = 'SELECT product.id,product_img,product_name,category_name,details,price,sales_count,inventory,product.create_time FROM product INNER JOIN classify INNER JOIN inventory ON product.classify_id = classify.id AND product.id = inventory.product_id';
 let data = new Array();
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res){
   connection.query(show_sql,(err,result,fields) => {
     if(err){
       console.log(err.message);
@@ -16,6 +16,26 @@ router.get('/', function(req, res) {
   });
   res.render('table1',{
     list:data
+  });
+});
+
+router.delete('/del/:id',(req,res) => {
+  connection.query('DELETE FROM product WHERE id='+req.params.id,(err,result,fields) =>{
+    if(err){
+      console.log(err.message);
+    }else{
+      delete data[req.params.id];
+      connection.query(show_sql,(err,result,fields) => {
+        if(err){
+          console.log(err.message);
+        }else{
+          data = JSON.parse(JSON.stringify(result));
+        }
+      });
+      res.render('table1',{
+        list:data
+      });    
+    }
   });
 });
 
