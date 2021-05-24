1c35fd826be8f7869147c0d2a9c7d7bed1d3a84f
许永健 <1942049754@qq.com>
2021-05-24 22:05:29 +0800

    change2

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -2,14 +2,20 @@ $(function () {
     Array.from($('.del')).forEach(i =>
         i.onclick = function () {
             let id = $(i).parent().parent().attr("datakey");
-            $.ajax({
-                url: "/table1/del/" + id,
-                type: "DELETE",
-                success: function () {
-                    alert("删除成功");
-                    $(i).parent().parent().remove();
-                }
-            });
+            let r = confirm('是否确认删除?');
+            if (r == true) {
+                $.ajax({
+                    url: "/table1/del/" + id,
+                    type: "DELETE",
+                    success: function () {
+                        alert("删除成功");
+                        $(i).parent().parent().remove();
+                    }
+                });
+            } else {
+                alert('取消删除');
+            }
+
         });
 });
 
--- routes/table1.js
+++ routes/table1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var connection = require('./conmysql')
 
 
-let show_sql = 'SELECT product.id,product_img,product_name,category_name,details,price,sales_count,inventory,product.create_time FROM product INNER JOIN classify INNER JOIN inventory ON product.classify_id = classify.id AND product.id = inventory.product_id';
+let show_sql = 'SELECT product.id,product_img,product_name,category_name,details,price,sales_count,inventory,product.create_time FROM product INNER JOIN classify  ON product.classify_id = classify.id';
 let data = new Array();
 
 router.get('/', function (req, res) {
@@ -48,24 +48,11 @@ router.post('/add', (req, res) => {
       console.log(err.message);
     } else {
       classify_id = JSON.parse(JSON.stringify(result));
-      connection.query('INSERT INTO product (product.classify_id,product.product_name,product.details,product.price,product.sales_count) VALUES(?,?,?,?,?)', [classify_id[0].id, req.body.product_name, req.body.details, req.body.price, req.body.sales_count], (err, result, fields) => {
+      connection.query('INSERT INTO product (product.classify_id,product.product_name,product.details,product.price,product.sales_count,product.inventory) VALUES(?,?,?,?,?,?)', [classify_id[0].id, req.body.product_name, req.body.details, req.body.price, req.body.sales_count, req.body.inventory], (err, result, fields) => {
         if (err) {
           console.log(err.message);
         } else {
-          connection.query('SELECT id FROM product WHERE product_name=?', [req.body.product_name], (err, result, fields) => {
-            if (err) {
-              console.log(err.message);
-            } else {
-              product_name = JSON.parse(JSON.stringify(result));
-              connection.query('INSERT INTO inventory (product_id,inventory) VALUES(?,?)', [product_name[0].id, req.body.inventory], (err, result, fields) => {
-                if (err) {
-                  console.log(err.message);
-                } else {
-                  res.redirect('/table1');
-                }
-              });
-            }
-          })
+          res.redirect('/table1');
         }
       });
     }
@@ -82,37 +69,17 @@ router.get('/chapage/:id', (req, res) => {
   }
 });
 
-router.post('/cha',(req,res) => {
-  connection.query('SELECT id FROM product WHERE product_name=?', [req.body.product_name], (err, result, fields) => {
+router.post('/cha', (req, res) => {
+  connection.query('SELECT id FROM classify WHERE category_name=?', [req.body.category_name], (err, result, fields) => {
     if (err) {
       console.log(err.message);
     } else {
-      let product_id = JSON.parse(JSON.stringify(result));
-      connection.query('SELECT id FROM classify WHERE category_name=?', [req.body.category_name], (err, result, fields) => {
+      let classify_id = JSON.parse(JSON.stringify(result));
+      connection.query('UPDATE product SET product_name=?,product.classify_id=?,product.details=?,product.price=?,product.sales_count=?,product.inventory=? WHERE product.id=?', [req.body.product_name, classify_id[0].id, req.body.details, req.body.price, req.body.sales_count, req.body.inventory, req.body.id], (err, result, fields) => {
         if (err) {
           console.log(err.message);
         } else {
-          let classify_id = JSON.parse(JSON.stringify(result));
-          connection.query('UPDATE product SET product.classify_id=?,product.details=?,product.price=?,product.sales_count=? WHERE product.id=?', [classify_id[0].id, req.body.details, req.body.price, req.body.sales_count, product_id[0].id], (err, result, fields) => {
-            if (err) {
-              console.log(err.message);
-            } else{
-              connection.query('SELECT id FROM inventory WHERE product_id=' + product_id[0].id, (err, result, fields) => {
-                if (err) {
-                  console.log(err.message);
-                } else{
-                  inventory_id = JSON.parse(JSON.stringify(result));
-                  connection.query('UPDATE inventory SET product_id=?,inventory=? WHERE inventory.id=?',[product_id[0].id,req.body.inventory,inventory_id[0].id],(err, result, fields) => {
-                    if (err) {
-                      console.log('c', err.message);
-                    } else {
-                      res.redirect('/table1');
-                    }
-                  })
-                }
-              })
-            }
-          })
+          res.redirect('/table1');
         }
       })
     }
