5693befe09682dab11a44951d4455ae8e65b2ede
许永健 <1942049754@qq.com>
2021-05-28 09:14:32 +0800

    shop_show

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('./conmysql');
 
-router.get('/', function(req, res) {
-  res.render('shop');
+let show_sql = 'SELECT product_img,product_name,price,intro FROM product';
+let data = new Array();
+
+router.get('/', function (req, res) {
+  connection.query(show_sql, (err, result, fields) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      data = JSON.parse(JSON.stringify(result));
+      res.render('shop', {
+        list: data
+      });
+    }
+  });
 });
 
 module.exports = router;
